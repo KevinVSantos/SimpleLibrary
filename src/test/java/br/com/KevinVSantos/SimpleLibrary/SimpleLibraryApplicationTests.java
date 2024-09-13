fe1c6d855cac7f20980966ef117037d7df2cbdd6
Kevin Santos <kevinsantos12111@gmail.com>
2024-09-13 15:15:47 -0300

    Inital commit

---
+++ src/test/java/br/com/KevinVSantos/SimpleLibrary/SimpleLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.KevinVSantos.SimpleLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
